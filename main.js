08f228bb32eeace08c5342bd01bb12981ecb8180
Sean Gravener <sean@gravener.net>
2022-09-13 17:48:42 -0400

    Draw a line

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("app-canvas");
+const ctx = canvas.getContext("2d");
 
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-})
+adjustCanvasSite();
+window.addEventListener("resize", () => {
+  adjustCanvasSite();
+});
 
 const mouse = {
-  x: null, 
-  y: null
+  x: null,
+  y: null,
+};
+
+function drawShape(radius = 0) {
+  const { w, h } = getCenter();
+
+  ctx.beginPath();
+  ctx.moveTo(w, h);
+  ctx.lineTo(w, h - radius);
+  ctx.closePath();
+  ctx.stroke();
+}
+drawShape(100);
+
+function getCenter() {
+  return { w: canvas.width / 2, h: canvas.height / 2 };
 }
 
-function drawCircle() {
-  
+function adjustCanvasSite() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
+
+console.log("script end");
